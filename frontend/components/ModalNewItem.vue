4728f6db1227567c8f2fd3d11a7c78951bc6c7c8
Danil Fedorov <danildimaf@gmail.com>
2023-04-05 19:43:07 +1000

    добавление товара (WIP)

---
+++ frontend/components/ModalNewItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <form class="max-w-md m-auto">
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-base font-semibold leading-7 text-gray-900">Добавление товара</h2>
+                <div class="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="product_title" class="block text-sm font-medium leading-6 text-gray-900">Название</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input type="text" name="product_title" id="product_title" autocomplete="product_title"
+                                    class="block flex-1 rounded py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="Новый Продукт" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-span-full">
+                        <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Описание</label>
+                        <div class="mt-2">
+                            <textarea id="description" name="description" rows="3"
+                                class="block w-full rounded text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+                                placeholder="Напишите что-нибудь веселое тут. Все равно это не настоящий онлайн магазин." />
+                        </div>
+                    </div>
+
+                    <div class="col-span-full">
+                        <label for="product-image" class="block text-sm font-medium leading-6 text-gray-900"> Изображение </label>
+                        <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                            <div class="text-center">
+                                <PhotoIcon class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                                <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                                    <label for="file-upload"
+                                        class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                        <span> Загрузить изображение </span>
+                                        <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                                    </label>
+                                    <p class="pl-1"> или перенестие его. </p>
+                                </div>
+                                <p class="text-xs leading-5 text-gray-600">в формате PNG, JPG, GIF до 10MB</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+            <button type="submit"
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import { PhotoIcon } from '@heroicons/vue/24/solid'
+</script>
